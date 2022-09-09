951d9cac294ccf7fe980006ae82dd3816ee1be50
Joao Szerman <joaoszerman@gmail.com>
2022-09-09 15:15:20 -0300

    first commit

---
+++ lib/screens/categories_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView(
+      gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 20,
+        mainAxisExtent: 20,
+      ),
+      children: [],
+    );
+  }
+}
